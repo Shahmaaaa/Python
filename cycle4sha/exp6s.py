dc5b97b63177f2738c64eebe76055656629557d5
Shahmaaaa <24mp2286@rit.ac.in>
2024-12-07 14:47:56 +0530

    Add files via upload

---
+++ cycle4sha/exp6s.py
@@ -0,0 +1,11 @@
+t=int(input("enter number of terms :"))
+terms_lst=[]
+for i in range(t):
+        terms=int(input("enter the terms :"))
+        terms_lst.append(terms)
+multiplefn=filter(lambda x:x%3==0,terms_lst)
+print("multiples of 3 are:")
+mulfnlst=list(multiplefn)
+print(mulfnlst)
+
+
